280c232342a14a7debd5b7e9564793ad71babb4c
AntiValve <antivalve@mail.ru>
2015-10-17 22:34:01 +0300

    Add namespaces via composer

---
+++ README.md
@@ -0,0 +1,21 @@
+# php development box
+Simple box for quick start.
+## Features:
+- ubuntu/trusty64 (Ubuntu 14.04 x64)
+- PHP 5.6
+- MySQL
+- Simple URL rewriter
+
+## Requirements
+- VirtualBox
+- Vagrant
+- vagrant-hostsupdater plugin (`vagrant plugin install vagrant-hostsupdater` to instal)
+- VT-x enabled
+
+### Windows:
+- Git bash with system path installation
+- Running vagrant with root permissions
+
+## Installation
+
+Run `git clone https://github.com/rst4d/php-box`, then enter `php-box` dir and run `vagrant up`. After box downloading and installaton you can visit [http://local.dev](http://local.dev).
---
+++ util/vg_deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export base_dir=/home/vagrant/local.dev
+
+export DBPASSWD=root
+
+if [ ! -f ~/.deploy_run ]
+then
+    echo "Add Vagrant user to the sudoers group..."
+    echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+    echo "Set MySQL root password..."
+    echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
+    echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
+
+    echo "Installing packages..."
+    apt-get update
+     curl -sL https://deb.nodesource.com/setup_4.x | bash -
+    apt-get -q -y install nodejs software-properties-common vim git curl
+    apt-get -q -y install mysql-server nginx
+    apt-get -q -y install php5-cli php5-fpm php5-mysql php5-curl php5-mcrypt
+
+    echo "Installing composer..."
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+
+    echo "Setting permissions..."
+    usermod -G vagrant www-data
+    usermod -G www-data vagrant
+
+    echo "Customizing nginx..."
+    service nginx stop
+    cp $base_dir/util/vg_nginx.conf /etc/nginx/sites-available/local.dev
+    ln -s /etc/nginx/sites-available/local.dev /etc/nginx/sites-enabled/local.dev
+    unlink /etc/nginx/sites-enabled/default
+    service nginx start
+
+    echo "Create DB..."
+    mysql -u root -p$DBPASSWD -e "CREATE DATABASE local CHARACTER SET utf8 COLLATE utf8_general_ci;"
+
+    touch ~/.deploy_run
+fi
