5c1611c19221a3872f6ac87760bf51a54aaad7f4
rst <uppyaleks@gmail.com>
2015-10-18 02:45:20 +0300

    CallMethod class

--- code/App/Controllers/IndexController.php
+++ code/App/Controllers/IndexController.php
@@ -4,4 +4,9 @@ namespace App\Controllers;
 
 class IndexController
 {
+    public function index()
+    {
+        $title = "Market";
+        require_once __DIR__.'/../../resources/views/index.php';
+    }
 }
---
+++ code/App/Library/CallMethod.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Library;
+
+class CallMethod
+{
+    public static function call($class, $method)
+    {
+        if(class_exists($class))
+        {
+            $object = new $class();
+            if(method_exists($object, $method))
+            {
+                call_user_func(array($object, $method));
+            }
+        }
+    }
+}
