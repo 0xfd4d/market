a50759e0c923cc4b6d531e64c1f52f2665eab233
AntiValve <antivalve@mail.ru>
2015-10-18 21:26:27 +0300

    Rename all view files to *.view.php format and added cart.

---
+++ code/resources/views/index.view.php
@@ -0,0 +1,25 @@
+<?php
+    use App\Library\View;
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <title>Home - Market</title>
+</head>
+<body>
+    <?php View::view('navbar'); ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Hello wolrd</h1>
+            </div>
+        </div>
+    </div>
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
--- code/resources/views/navbar.view.php
+++ code/resources/views/navbar.view.php
@@ -15,6 +15,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li><a href="/">Home</a></li>
+                <li><a href="/cart">Cart</a></li>
                 <!-- <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                     <ul class="dropdown-menu">
@@ -28,10 +29,10 @@
                     </ul>
                 </li> -->
             </ul>
-            <ul class="nav navbar-nav navbar-right">
+            <!-- <ul class="nav navbar-nav navbar-right">
                 <li><a href="/auth/login">Login</a></li>
                 <li><a href="/auth/register">Register</a></li>
-            </ul>
+            </ul> -->
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
