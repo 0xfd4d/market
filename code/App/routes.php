463dfaa3b582ac80fd3b07502942558c11c93ebf
fd4d <fd4d@users.noreply.github.com>
2015-10-18 18:42:01 +0300

    CallMethod::call now can accept arguments.

--- code/App/routes.php
+++ code/App/routes.php
@@ -8,6 +8,10 @@ Route::add('/', function() {
     CallMethod::call('\\App\\Controllers\\IndexController', 'index');
 });
 
+Route::add('/product', function() {
+    CallMethod::call('\\App\\Controllers\\IndexController', 'test', [1]);
+});
+
 /**
  * Call 404 error if none of this routes not run.
  */
