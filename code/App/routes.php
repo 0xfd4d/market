1f09b154649c9cfa21e4c008f2bd39b02dc3cc74
AntiValve <antivalve@mail.ru>
2015-10-18 21:14:15 +0300

    Route->add method now accepts class and method instead of running Closure function.
    Delete Request.

--- code/App/routes.php
+++ code/App/routes.php
@@ -6,13 +6,9 @@ use App\Library\CallMethod;
 
 $route = new Route();
 
-$route->add('/', function($request) {
-    CallMethod::call('\\App\\Controllers\\IndexController', 'index', []);
-});
+$route->add('/', '\\App\\Controllers\\IndexController', 'index');
 
-$route->add('/test//', function($request) {
-    CallMethod::call('\\App\\Controllers\\IndexController', 'test', [$request]);
-});
+$route->add('/test', '\\App\\Controllers\\IndexController', 'test');
 
 /**
  * Call 404 error if none of this routes not run.
