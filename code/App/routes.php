1f52723c1f2bd7c5c350f991f0f59c5189238066
fd4d <fd4d@users.noreply.github.com>
2015-10-19 05:45:37 +0300

    Add params in Request

--- code/App/routes.php
+++ code/App/routes.php
@@ -2,12 +2,17 @@
 
 use App\Library\View;
 use App\Library\Route;
+use App\Library\Request;
 use App\Library\CallMethod;
 
 Route::init();
 
 Route::add('', '\\App\\Controllers\\IndexController', 'index');
 Route::add('/cart', '\\App\\Controllers\\CartController', 'index');
+
+Route::addCallback('/test/(.+)/test/(.+)', function(Request $request) {
+    print_r($request->params);
+});
 
 /**
  * 404 error.
