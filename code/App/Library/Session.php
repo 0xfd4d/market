297fffffdb74876c104753146dcd5a2fe62b0ec6
fd4d <fd4d@users.noreply.github.com>
2015-10-20 19:01:15 +0300

    Fixed session folder storage bug

--- code/App/Library/Session.php
+++ code/App/Library/Session.php
@@ -6,7 +6,6 @@ class Session
 {
     public static function init()
     {
-        session_save_path(__DIR__.'/../../resources/sessions');
         session_name('market_session');
         session_start();
     }
