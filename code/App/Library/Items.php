5bc81ce867fc6d87a7878a7bf0c1b52d48d45a86
fd4d <fd4d@users.noreply.github.com>
2015-10-22 21:18:00 +0300

    Add create items form
    Add Auth::isAdmin()
    
    Fix routes

--- code/App/Library/Items.php
+++ code/App/Library/Items.php
@@ -2,11 +2,13 @@
 
 namespace App\Library;
 
+use App\Library\Request;
+
 class Items
 {
     public static function getItems()
     {
-        $query = DB::$db->prepare('SELECT * FROM items');
+        $query = DB::$db->prepare('SELECT * FROM items ORDER BY id desc');
         $query->execute();
         $result = $query->fetchAll();
         return $result;
@@ -16,6 +18,15 @@ class Items
         $query = DB::$db->prepare('INSERT INTO items (name, price, image, description) VALUES (?, ?, ?, ?)');
         $query->execute([$request->post['name'], $request->post['price'], $request->post['image'], $request->post['description']]);
         $result = $query->fetch();
-        return $result;
+        return $result[0];
+    }
+    public static function validateCreateItem($request)
+    {
+        $errors = [];
+        if(!$request->hasPost('name') || !$request->hasPost('price') || !$request->hasPost('image') || !$request->hasPost('description'))
+        {
+            $errors[] = 'All fields required';
+        }
+        return $errors;
     }
 }
