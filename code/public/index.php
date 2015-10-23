0c3d2edb571a0963b66c213b768665fa40faeac5
fd4d <fd4d@users.noreply.github.com>
2015-10-23 23:48:33 +0300

    Fix bug when seed.php cant connect to db

--- code/public/index.php
+++ code/public/index.php
@@ -5,8 +5,5 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use App\Library\Bootstrap;
 
-$dotenv = new Dotenv\Dotenv(__DIR__.'/../');
-$dotenv->load();
-
 $bootstrap = new Bootstrap();
 $bootstrap->run();
