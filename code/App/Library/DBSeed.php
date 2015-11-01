e172446c1f3de5c0cacc8eb7f48b6ba5ea5af236
rst <uppyaleks@gmail.com>
2015-11-01 17:27:37 +0300

    Add HEAVYWEAPONS DB SEED

--- code/App/Library/DBSeed.php
+++ code/App/Library/DBSeed.php
@@ -27,6 +27,12 @@ class DBSeed
                 'image' => 'http://www.logitech.com/assets/26704/26704.png',
                 'description' => 'Office Keyboard',
             ],
+            [
+                'name' => 'Heavy\'s minigun',
+                'price' => '400000',
+                'image' => 'https://wiki.teamfortress.com/w/images/thumb/4/42/Minigun_IMG.png/375px-Minigun_IMG.png',
+                'description' => 'Она весит сто пятьдесят килограмм и делает десять тысяч выстрелов в минуту патронами по двести баксов каждый. Нужно четыреста тысяч долларов, чтобы стрелять из этого пулемета... двенадцать секунд.',
+                ]
         ];
         $newitem = DB::$db->prepare('INSERT INTO items (name, price, image, description) VALUES (?, ?, ?, ?)');
         foreach ($items as $item) {
